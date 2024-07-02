fbce29d47a5e0f73f89d2da988a54f9bc04f8f6c
Sean Chou <sean@choufam.com>
2024-07-02 12:41:47 -0500

    Add better/global theming support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,43 +1,12 @@
--- Set leader key to space
--- vim.g.mapleader = ' '
+-- init.lua
 
+-- Theme configuration
+-- Set this variable to "gruvbox", "nord", or "tokyonight" to change the theme
+local chosen_theme = "tokyonight"
+
+-- Source .vimrc for backward compatibility
 vim.cmd('source ~/.vimrc')
 
--- vim.cmd('colorscheme lunaperche')
--- Set nvim to transparent background
---vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
-
--- General settings
--- local options = {
---     expandtab = true,
---     tabstop = 4,
---     shiftwidth = 4,
---     softtabstop = 4,
---     number = true,
---     relativenumber = true,
---     cursorline = true,
---     mouse = 'a',
---     backup = false,
---     writebackup = false,
---     undofile = true,
---     ignorecase = true,
---     smartcase = true,
---     incsearch = true,
---     hlsearch = true,
---     cmdheight = 2,
---     autoindent = true,
---     smartindent = true,
---     splitright = true,
---     splitbelow = true,
---     updatetime = 300,
---     clipboard = 'unnamedplus'
--- }
---
--- -- Apply each option
--- for k, v in pairs(options) do
---     vim.o[k] = v
--- end
---
 -- Lazy package manager setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -52,66 +21,59 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Plugin management with lazy.nvim
+-- Plugin definitions
 local plugins = {
-    -- Nord
+    -- Color schemes
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = true,
+        opts = {},
+    },
     {
         "gbprod/nord.nvim",
-        lazy = false,
         priority = 1000,
         config = function()
             require("nord").setup({})
-            vim.cmd.colorscheme("nord")
         end,
-        install = {
-            colorscheme = { "nord" },
-        },
     },
-    -- Gruvbox
-    { 
-        "ellisonleao/gruvbox.nvim", 
-        priority = 1000, 
-        config = true, 
-        opts = {} 
-    },
-    -- Tokyonight
     {
         "folke/tokyonight.nvim",
-        lazy = false,
         priority = 1000,
         opts = {},
     },
-     -- Lualine statusline
+
+    -- UI enhancements
     {
         'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+        dependencies = { 'kyazdani42/nvim-web-devicons', opt = true }
     },
-    -- Treesitter for enhanced syntax highlighting
+
+    -- Syntax and language support
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
-            require 'nvim-treesitter.configs'.setup {
+            require('nvim-treesitter.configs').setup {
                 ensure_installed = { "c", "lua", "vim", "python", "javascript", "html" },
                 sync_install = false,
                 auto_install = true,
                 highlight = {
                     enable = true,
-                    disable = {},
                     additional_vim_regex_highlighting = false,
                 },
             }
         end
     },
-    -- LSP configuration
+
+    -- LSP and autocompletion
     {
         "neovim/nvim-lspconfig",
         config = function()
-            require 'lspconfig'.pyright.setup {}
-            require 'lspconfig'.tsserver.setup {}
+            require('lspconfig').pyright.setup {}
+            require('lspconfig').tsserver.setup {}
         end
     },
-    -- nvim-cmp for autocompletion
     {
         "hrsh7th/nvim-cmp",
         dependencies = {
@@ -123,7 +85,7 @@ local plugins = {
             "hrsh7th/cmp-vsnip",
         },
         config = function()
-            local cmp = require 'cmp'
+            local cmp = require('cmp')
             cmp.setup {
                 snippet = {
                     expand = function(args)
@@ -141,15 +103,16 @@ local plugins = {
                     }),
                 },
                 sources = {
-                    { name = 'nvim_lsp' },  -- LSP completions
-                    { name = 'vsnip' },     -- Snippet completions
-                    { name = 'buffer' },    -- Buffer completions
-                    { name = 'path' },      -- File path completions
+                    { name = 'nvim_lsp' },
+                    { name = 'vsnip' },
+                    { name = 'buffer' },
+                    { name = 'path' },
                 }
             }
         end
     },
-    -- Telescope for fuzzy finding
+
+    -- Fuzzy finder
     {
         "nvim-telescope/telescope.nvim",
         tag = '0.1.6',
@@ -157,25 +120,49 @@ local plugins = {
     },
 }
 
+-- Initialize lazy.nvim
 require("lazy").setup(plugins, {})
 
--- Set color scheme
--- vim.cmd[[colorscheme nord]]
--- vim.cmd[[colorscheme gruvbox]]
-vim.cmd[[colorscheme tokyonight-night]]
+-- Theme application function
+function _G.apply_theme(theme)
+    vim.o.background = "dark"
+    local colorscheme = theme
+    local lualine_theme = theme
 
--- Lualine setup
-require('lualine').setup {
-    options = { 
-        -- theme = 'gruvbox' 
-        -- theme = 'nord' 
-        theme = 'tokyonight' 
+    if theme == "tokyonight" then
+        colorscheme = "tokyonight-night"
+        -- lualine_theme remains "tokyonight"
+    end
+
+    vim.cmd("colorscheme " .. colorscheme)
+
+    -- Lualine setup
+    require('lualine').setup {
+        options = { 
+            theme = lualine_theme
+        }
     }
-}
+end
 
--- Custom key mappings for Telescope
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', opts)
+-- Apply the chosen theme
+apply_theme(chosen_theme)
+
+-- Create a user command for easier theme switching
+vim.api.nvim_create_user_command('Theme', function(opts)
+    apply_theme(opts.args)
+end, { nargs = 1, complete = function()
+    return { "gruvbox", "nord", "tokyonight" }
+end })
+
+-- Keymappings
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then options = vim.tbl_extend('force', options, opts) end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Telescope keymappings
+map('n', '<leader>ff', ':Telescope find_files<CR>')
+map('n', '<leader>fg', ':Telescope live_grep<CR>')
+map('n', '<leader>fb', ':Telescope buffers<CR>')
+map('n', '<leader>fh', ':Telescope help_tags<CR>')
