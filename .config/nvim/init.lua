303ea85c1aa6f2c20e278af6908d8d4a95273f73
Sean Chou <sean@choufam.com>
2024-07-21 23:11:34 -0500

    Add netrw config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,3 @@
--- init.lua
-
--- Theme configuration
--- Supported themes:
---     catppuccin
 --     catppuccin-latte
 --     catppuccin-frappe
 --     catppuccin-macchiato
@@ -21,7 +16,7 @@ vim.cmd('source ~/.vimrc')
 
 -- Lazy package manager setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then -- type: ignore
     vim.fn.system({
         "git",
         "clone",
@@ -265,8 +260,6 @@ local plugins = {
                 mapping = {
                     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-j>'] = cmp.mapping.select_next_item(),
-                    ['<C-k>'] = cmp.mapping.select_prev_item(),
                     ['<C-l>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.close(),
                     ['<CR>'] = cmp.mapping.confirm({
@@ -315,7 +308,6 @@ local plugins = {
             'csv',
             'tsv',
             'csv_semicolon',
-            'csv_whitespace',
             'csv_pipe',
             'rfc_csv',
             'rfc_semicolon'
@@ -341,6 +333,11 @@ local plugins = {
                 -- custom which-key setup
             })
             wk.register({
+                e = {
+                    name = "Explore",
+                    e = { "<leader>dd :Lexplore %:p:h<CR>", "Explore Dir of File" },
+                    a = { "<leader>dd :Lexplore<CR>", "Explore Current Dir" },
+                },
                 f = {
                     name = "Find",
                     f = { "<cmd>Telescope find_files<CR>", "Find File" },
@@ -353,9 +350,11 @@ local plugins = {
                     f = { function() vim.lsp.buf.format({ async = true }) end, "Format" },
                     e = { vim.diagnostic.open_float, "Open Float" },
                     q = { vim.diagnostic.setloclist, "Set Loclist" },
+                    d = { vim.lsp.buf.definition, "Definition" },
                     D = { vim.lsp.buf.type_definition, "Type Definition" },
                     r = { vim.lsp.buf.rename, "Rename" },
                     a = { vim.lsp.buf.code_action, "Code Action" },
+                    s = { vim.lsp.buf.signature_help, "Signature Help" },
                     w = {
                         name = "Workspace",
                         a = { vim.lsp.buf.add_workspace_folder, "Add Folder" },
@@ -365,23 +364,22 @@ local plugins = {
                 },
                 r = {
                     name = "Run",
+                    c = { "<cmd>source $MYVIMRC<CR>", "Reload Config" },
                     p = { "<cmd>RunPython<CR>", "Run Python" },
                 },
-                h = {
-                    name = "Git Hunk",
+                G = {
+                    name = "Gitsigns",
                     s = { "<cmd>Gitsigns stage_hunk<CR>", "Stage Hunk" },
                     u = { "<cmd>Gitsigns undo_stage_hunk<CR>", "Undo Stage Hunk" },
                     r = { "<cmd>Gitsigns reset_hunk<CR>", "Reset Hunk" },
                     p = { "<cmd>Gitsigns preview_hunk<CR>", "Preview Hunk" },
                     b = { "<cmd>Gitsigns blame_line<CR>", "Blame Line" },
-                },
-                ["["] = {
-                    c = { "<cmd>Gitsigns prev_hunk<CR>", "Previous Git Hunk" },
-                },
-                ["]"] = {
-                    c = { "<cmd>Gitsigns next_hunk<CR>", "Next Git Hunk" },
+                    a = { "<cmd>GitAdd<CR>", "Git Add" },
+                    c = { "<cmd>GitCommit<CR>", "Git Commit" },
+                    d = { "<cmd>GitDiff<CR>", "Git Diff" },
                 },
             }, { prefix = "<leader>" })
+
 
             -- Non-leader keymaps
             wk.register({
@@ -390,9 +388,10 @@ local plugins = {
                 gi = { vim.lsp.buf.implementation, "Go to Implementation" },
                 gr = { vim.lsp.buf.references, "References" },
                 K = { vim.lsp.buf.hover, "Hover Information" },
-                ["<C-k>"] = { vim.lsp.buf.signature_help, "Signature Help" },
                 ["[d"] = { vim.diagnostic.goto_prev, "Previous Diagnostic" },
                 ["]d"] = { vim.diagnostic.goto_next, "Next Diagnostic" },
+                ["[c"] = { "<cmd>Gitsigns prev_hunk<CR>", "Previous Git Hunk" },
+                ["]c"] = { "<cmd>Gitsigns next_hunk<CR>", "Next Git Hunk" },
             })
         end
     },
@@ -459,6 +458,40 @@ vim.api.nvim_create_autocmd("FileType", {
     end
 })
 
+-- Netrw keymappings (dupe from .vimrc for which-key menu)
+--[[
+local wk = require("which-key")
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "netrw",
+    callback = function()
+        wk.register({
+            ["<Leader>"] = {
+                d = { "<cmd>Lexplore<CR>", "Close netrw" },
+                b = {
+                    name = "Bookmark",
+                    b = { "mb", "Create bookmark" },
+                    d = { "mB", "Remove most recent bookmark" },
+                    l = { "gb", "Jump to most recent bookmark" },
+                },
+            },
+            f = {
+                name = "File",
+                f = { "%:w<CR><cmd>buffer #<CR>", "Create file" },
+                e = { "R", "Rename file" },
+                c = { "mc", "Copy marked files" },
+                C = { "mtmc", "Mark, move cursor, and copy" },
+                x = { "mm", "Move marked files" },
+                X = { "mtmm", "Same as fC but for moving files" },
+                [";"] = { "mx", "Run commands on marked files" },
+                l = { "<cmd>echo join(netrw#Expose('netrwmarkfilelist'), '\n')<CR>",
+                    "Show list of all marked files" },
+            },
+        }, { buffer = 0 }) -- This ensures the mappings only apply to the current (netrw) buffer
+    end
+})
+]] --
+
 -- Create a command to run Python scripts
 vim.api.nvim_create_user_command('RunPython', function()
     vim.cmd('w')                  -- Save the file
@@ -466,10 +499,29 @@ vim.api.nvim_create_user_command('RunPython', function()
     vim.cmd('terminal python3 %') -- Run the current file in the terminal
 end, {})
 
--- Remap Ctrl-N to Ctrl-J and Ctrl-P to Ctrl-K in command-line mode
-vim.cmd([[
-  cnoremap <C-J> <C-N>
-  cnoremap <C-K> <C-P>
-]])
+-- Git commands
+vim.api.nvim_create_user_command('GitAdd', function()
+    vim.cmd('!git add %')
+end, {})
+
+vim.api.nvim_create_user_command('GitCommit', function()
+    local message = vim.fn.input("Commit message: ")
+    if message ~= "" then
+        local result = vim.fn.system('git commit -m "' .. message .. '"')
+        print(result)
+    else
+        print("Commit cancelled: No message provided")
+    end
+end, {})
+
+vim.api.nvim_create_user_command('GitDiff', function()
+    vim.cmd('vsplit')                                  -- Create a vertical split
+    vim.cmd('enew')                                    -- Create a new buffer in the new split
+    vim.cmd('setlocal buftype=nofile')                 -- Set the buffer type to nofile
+    vim.cmd('read !git diff ' .. vim.fn.expand('#:p')) -- Read the diff into the buffer
+    vim.cmd('setlocal nomodifiable')                   -- Make the buffer non-modifiable
+    vim.cmd('setfiletype diff')                        -- Set the filetype to diff for syntax highlighting
+    vim.cmd('normal! gg')                              -- Move cursor to the top of the buffer
+end, {})
 
 -- End of init.lua
--- .vimrc
+++ .vimrc
@@ -1,6 +1,4 @@
-" .vimrc (also sourced by nvim)
 
-" Use spaces instead of tabs
 set expandtab
 
 " Number of spaces that a <Tab> in the file counts for
@@ -75,3 +73,56 @@ set background=dark
 " Change the default mapleader
 let mapleader=" "
 
+" Netrw settings
+
+" Sync current dir and browsing dir
+let g:netrw_keepdir = 0
+
+" Set split size to 25%
+let g:netrw_winsize = 25
+
+" Hide banner (use I to toggle)
+let g:netrw_banner = 1      " Set to 0 to not show
+
+" Hide dotfiles by default
+let g:netrw_list_hide = '\(^\|\s\s\)\zs\.\S\+'
+
+" Recursively copy dirs
+let g:netrw_localcopydircmd = 'cp -r'
+
+" Highlight marked files
+hi! link netrwMarkFile Search
+
+" Keymappings for netrw
+"function! NetrwMapping()
+"    nmap <buffer> h -^                       " Go up
+"    nmap <buffer> l <CR>                     " Open file / cd
+"
+"    nmap <buffer> . gh                       " Toggle dotfiles
+"    nmap <buffer> P <C-w>z                   " Close preview window
+"
+"    nmap <buffer> L <CR>:Lexplore<CR>        " Open file and close netrw
+"    nmap <buffer> <Leader>dd :Lexplore<CR>   " Close netrw
+"
+"    nmap <buffer> <TAB> mf                   " Toggle mark on file
+"    nmap <buffer> <S-TAB> mF                 " Unmark all files in current buf
+"    nmap <buffer> <Leader><TAB> mu           " Remove all marks on all files
+"
+"    nmap <buffer> fe R                       " Rename file
+"    nmap <buffer> fc mc                      " Copy marked files      
+"    nmap <buffer> fC mtmc                    " Mark, move cursor, and copy
+"    nmap <buffer> fx mm                      " Move marked files
+"    nmap <buffer> fX mtmm                    " Same a fC but for moving files
+"    nmap <buffer> f; mx                      " Run commands on marked files
+"    " Show list of all marked files
+"    nmap <buffer> fl :echo join(netrw#Expose("netrwmarkfilelist"), "\n")<CR>
+"
+"    nmap <buffer> bb mb                      " Create bookmark
+"    nmap <buffer> bd mB                      " Remove most recent bookmark
+"    nmap <buffer> bl gb                      " Jump to most recent bookmark
+"endfunction
+"
+"augroup netrw_mapping
+"    autocmd!
+"    autocmd filetype netrw call NetrwMapping()
+"augroup END
