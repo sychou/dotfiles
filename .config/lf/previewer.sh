0690d94836bac50a53ca38186c2ce111bf894abf
Sean Chou <sean@choufam.com>
2026-02-15 15:00:33 -0600

    Add csvkit and lf config

--- .config/lf/previewer.sh
+++ .config/lf/previewer.sh
@@ -4,5 +4,6 @@ case "$1" in
     *.tar*) tar tf "$1" ;;
     *.zip)  unzip -l "$1" ;;
     *.pdf)  pdftotext "$1" - ;;
+    *.xlsx|*.xls) in2csv "$1" | bat --color=always --style=plain --paging=never --language=csv ;;
     *)      bat --color=always --style=plain --paging=never "$1" ;;
 esac
