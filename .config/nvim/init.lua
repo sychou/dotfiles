928c712e903e146ceb7392176f95fdcb1cc9bcf1
Sean Chou <sean@choufam.com>
2026-02-15 15:55:28 -0600

    Update breaking tree-sitter change

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -87,7 +87,7 @@ require("lazy").setup({
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
-            require('nvim-treesitter.configs').setup {
+            require('nvim-treesitter').setup {
                 ensure_installed = { "c", "lua", "vim", "python", "javascript", "html" },
                 sync_install = false,
                 auto_install = true,
